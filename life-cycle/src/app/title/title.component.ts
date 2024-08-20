a9bc0987528373394d8ceff468623d1d19f14479
LucasjsSilva <luquinhajssilva@ufpi.edu.br>
2024-08-20 17:07:02 -0300

    entendendo os hoocks Oninit e Onchange

--- life-cycle/src/app/title/title.component.ts
+++ life-cycle/src/app/title/title.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.css']
 })
-export class TitleComponent implements OnInit {
-
+export class TitleComponent implements OnInit, OnChanges {
+	@Input()
+	nome: string = ""
   constructor() { }
 
   ngOnInit(): void {
+		console.log("Inicializando")
   }
+
+	ngOnChanges(): void {
+		console.log("Houve Mudanças")
+	}
 
 }
