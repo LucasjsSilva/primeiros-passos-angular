c617c0aaf920d9f3e00a2e048997559e4986be87
LucasjsSilva <luquinhajssilva@ufpi.edu.br>
2024-08-21 00:06:54 -0300

    tentar semrpe organizar em modulos fica mais organizado uma outra maneira de organizar

---
+++ modulos/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ListaModule } from './lista/lista.module'
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ListaModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
