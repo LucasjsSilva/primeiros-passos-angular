e011f98a695712bf9f43cf5e2630f17d60b9d4fd
LucasjsSilva <luquinhajssilva@ufpi.edu.br>
2024-08-20 22:13:47 -0300

    diretiva ngswitch e switchcase criando bloocs que aparecem de acordo com uma variavel

--- diretivas-proj/src/app/card/card.component.ts
+++ diretivas-proj/src/app/card/card.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class CardComponent implements OnInit {
 
   produtos: string[] = []
-
+  menuType: string = "superuser"
   constructor() {
 
     this.produtos = [
